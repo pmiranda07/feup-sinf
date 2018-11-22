5d2db8f20add857f710653c263e1d4044369f3b1
Daniel Marques <up201503822@fe.up.pt>
2018-11-22 15:02:07 +0000

    Setup router

--- web/src/App.js
+++ web/src/App.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/financial/">financial</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route path="/" exact component={Overview} />
+          <Route path="/financial/" component={Financial} />
+          <Route path="/sales/" component={Sales} />
+          <Route path="/purchases/" component={Purchases} />
+          <Route path="/products/" component={Products} />
+        </div>
+      </Router>
     );
   }
 }
