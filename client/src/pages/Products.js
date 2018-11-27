c17d5b33d7df4b82b0b87476a19805270fc7d796
Daniel Marques <up201503822@fe.up.pt>
2018-11-27 19:11:24 +0000

    Product list done!

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
-import ProductListItem from '../components/ProductListItem';
 import axios from 'axios';
-import {Table} from 'react-bootstrap';
+import BootstrapTable from 'react-bootstrap-table-next';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
 
 class Products extends Component {
   constructor(props) {
@@ -27,28 +30,71 @@ class Products extends Component {
   };
 
   render() {
-    const products = this.state.products.map((product) => (
-      <ProductListItem {...product}></ProductListItem>
-    ));
+    const columns = [{
+        dataField: 'ProductCode',
+        text: 'Code',
+        sort: true
+      }, {
+        dataField: 'ProductNumberCode',
+        text: 'Number Code',
+        sort: true
+      }, {
+        dataField: 'ProductType',
+        text: 'Type',
+        sort: true
+      }, {
+        dataField: 'ProductGroup',
+        text: 'Group',
+        sort: true
+      }, {
+        dataField: 'ProductDescription',
+        text: 'Description',
+        sort: true,
+        events: {
+          onClick: (e, column, columnIndex, row, rowIndex) => { 
+            // TODO Navigate to product page
+          }
+        },
+        filter: textFilter({
+          delay: 50, 
+          style: {
+            display: 'none'
+          },
+          placeholder: 'Search product name',
+          getFilter: (filter) => {
+            this.nameFilter = filter;
+          }
+        })
+      }
+    ];
+
+    const defaultSorted = [{
+      dataField: 'ProductCode',
+      order: 'asc'
+    }];
+
+    const customTotal = (from, to, size) => (
+      <span className="react-bootstrap-table-pagination-total">
+        Showing { from } to { to } of { size } products
+      </span>
+    );
+
+    const options = {
+      hideSizePerPage: true,
+      hidePageListOnlyOnePage: true,
+      showTotal: true,
+      paginationTotalRenderer: customTotal
+    };
+
+    const handleSearchInput = (e) => {
+      this.nameFilter(e.target.value);
+    };
 
     return (
       <div>
         <h1>Products</h1>
-        <Table striped bordered condensed hover responsive>
-          <thead>
-            <tr>
-              <th>ProductCode</th>
-              <th>ProductNumberCode</th>
-              <th>ProductType</th>
-              <th>ProductGroup</th>
-              <th>ProductDescription</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {products}
-          </tbody>
-        </Table>
+        <input type="text" className="form-control" placeholder="Search product name" onInput={ handleSearchInput }/>
+        <BootstrapTable bootstrap4 striped hover keyField='ProductCode' data={ this.state.products } columns={ columns } defaultSorted={defaultSorted} pagination={paginationFactory(options)} filter={filterFactory()}/>
       </div>
     );
   }
