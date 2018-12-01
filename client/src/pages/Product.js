03aa004d86b86198781eb60640ec8fc1a340bede
Daniel Marques <up201503822@fe.up.pt>
2018-12-01 15:44:32 +0000

    Started working on product page

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import qs from 'qs';
 
 class Product extends Component {
     constructor(props) {
@@ -7,30 +8,71 @@ class Product extends Component {
         
         this.state = {
             id: props.match.params.id,
-            info: { }
+            info: { Descricao: '' }
         }
+
+        this.token = "";
     }
 
 
     componentDidMount() {
-        this.callAPI()
-            .then((res) => this.handleResponse(res))
+        this.getPrimaveraToken()
+            .then((res) => this.handleTokenResponse(res))
             .catch(err => console.log(err));
-      }
-    
-    callAPI = async () => {
-        return axios.get('/Products/' + this.state.id);
+    }
+
+    getPrimaveraToken = async () => {
+        return axios({
+            method: 'POST',
+            url: "http://localhost:2018/WebApi/token",
+            data: qs.stringify({
+            username: "FEUP",
+            password: "qualquer1",
+            company: "DEMO",
+            instance: "Default",
+            grant_type: "password",
+            line: "Professional"
+            }),
+            headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
     };
 
-    handleResponse(res) {
-        this.setState( { info: res.data.info } );
+    handleTokenResponse(res) {
+        if(res.data.access_token)
+          this.token = res.data.access_token;
+    
+        this.callPrimavera()
+          .then((res) => this.handlePrimaveraResponse(res))
+          .catch(err => console.log(err));
+    };
+    
+    callPrimavera = async () => {
+        var query = JSON.stringify("SELECT Artigo, Descricao, STKActual, PCMedio FROM Artigo WHERE Artigo = '" + this.state.id + "'");
+
+        return axios({
+          method: 'post',
+          url: 'http://localhost:2018/WebApi/Administrador/Consulta',
+          crossdomain: true,
+          headers: {
+            'content-type': 'application/json',
+            'authorization': "Bearer " + this.token
+          },
+          data: query
+        });
+    };
+
+    handlePrimaveraResponse(res) {
+        if ( res.status === 200 && res.data.DataSet.Table.length)
+            this.setState( { info: res.data.DataSet.Table[0] } );
     };
 
 
     render() {
         return (
             //TODO
-            <div>{this.state.info.ProductDescription}</div>
+            <div>{this.state.info.Descricao}</div>
         );
     }
 
