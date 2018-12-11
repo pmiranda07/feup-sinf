2b885754eb1adc39e9478f59551d3fd0dac68df0
Daniel Marques <up201503822@fe.up.pt>
2018-12-11 12:02:52 +0000

    Working on products

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import ReactLoading from 'react-loading';
+import { ResponsiveLine } from 'nivo';
 
 class Product extends Component {
     constructor(props) {
@@ -83,11 +84,203 @@ class Product extends Component {
             return this.renderLoading();
 
         return (
-            //TODO
-            <div>{this.state.info.Descricao}</div>
+            <div className="card">
+                <div className="card-header">
+                    <strong>Product: </strong>{this.state.info.Descricao}
+                </div>
+                <div className="card-body">
+                    <div className="d-flex flex-row justify-content-around">
+                        <span className="card w-25">
+                            <div className="card-header">ID</div>
+                            <div className="card-body">{this.state.info.Artigo}</div>
+                        </span>
+
+                        <span className="card w-25">
+                            <div className="card-header">Stock</div>
+                            <div className="card-body">{this.state.info.STKActual}</div>
+                        </span>
+
+                        <span className="card w-25">
+                            <div className="card-header">Price</div>
+                            <div className="card-body">{this.state.info.PCMedio}</div>
+                        </span>
+                    </div>
+
+                    <div style={{height: 500}}>
+                    <ResponsiveLine
+                      colors="set1"
+                      data={my_data}
+                      margin={{
+                          "top": 50,
+                          "right": 110,
+                          "bottom": 50,
+                          "left": 60
+                      }}
+                      xScale={{
+                          "type": "point"
+                      }}
+                      yScale={{
+                          "type": "linear",
+                          "stacked": false,
+                          "min": "auto",
+                          "max": "auto"
+                      }}
+                      axisTop={null}
+                      axisRight={null}
+                      axisBottom={{
+                          "orient": "bottom",
+                          "tickSize": 5,
+                          "tickPadding": 5,
+                          "tickRotation": 0,
+                          "legend": "Year",
+                          "legendOffset": 36,
+                          "legendPosition": "center"
+                      }}
+                      axisLeft={{
+                          "orient": "left",
+                          "tickSize": 5,
+                          "tickPadding": 5,
+                          "tickRotation": 0,
+                          "legend": "Value",
+                          "legendOffset": -40,
+                          "legendPosition": "center"
+                      }}
+                      dotSize={10}
+                      dotColor="inherit:darker(0.3)"
+                      dotBorderWidth={2}
+                      dotBorderColor="#ffffff"
+                      enableDotLabel={true}
+                      dotLabel="y"
+                      dotLabelYOffset={-12}
+                      animate={true}
+                      motionStiffness={90}
+                      motionDamping={15}
+                      legends={[
+                          {
+                              "anchor": "bottom-right",
+                              "direction": "column",
+                              "justify": false,
+                              "translateX": 100,
+                              "translateY": 0,
+                              "itemsSpacing": 0,
+                              "itemDirection": "left-to-right",
+                              "itemWidth": 80,
+                              "itemHeight": 20,
+                              "itemOpacity": 0.75,
+                              "symbolSize": 12,
+                              "symbolShape": "circle",
+                              "symbolBorderColor": "rgba(0, 0, 0, .5)",
+                              "effects": [
+                                  {
+                                      "on": "hover",
+                                      "style": {
+                                          "itemBackground": "rgba(0, 0, 0, .03)",
+                                          "itemOpacity": 1
+                                      }
+                                  }
+                              ]
+                          }
+                      ]}
+                  />
+                    </div>
+                </div>
+            </div>
         );
     }
 
 }
+
+const my_data = [
+    {
+      "id": "Purchases",
+      "color": "hsl(113, 70%, 50%)",
+      "data": [
+        {
+          "x": "2013",
+          "y": 0,
+        },
+        {
+          "x": "2014",
+          "y": 0,
+        },
+        {
+          "x": "2015",
+          "y": 0,
+        },
+        {
+          "x": "2016",
+          "y": 146
+        },
+        {
+          "x": "2017",
+          "y": 271
+        },
+        {
+          "x": "2018",
+          "y": 132
+        }
+      ]
+    },
+    {
+      "id": "Sales",
+      "color": "hsl(335, 70%, 50%)",
+      "data": [
+        {
+          "x": "2013",
+          "y": 0,
+        },
+        {
+          "x": "2014",
+          "y": 0,
+        },
+        {
+          "x": "2015",
+          "y": 0,
+        },
+        {
+          "x": "2016",
+          "y": 229
+        },
+        {
+          "x": "2017",
+          "y": 21
+        },
+        {
+          "x": "2018",
+          "y": 206
+        }
+      ]
+    },
+    {
+      "id": "Profit",
+      "color": "hsl(186, 70%, 50%)",
+      "data": [
+        {
+          "x": "2013",
+          "y": 0,
+        },
+        {
+          "x": "2014",
+          "y": 0,
+        },
+        {
+          "x": "2015",
+          "y": 0,
+        },
+        {
+          "x": "2016",
+          "y": 116
+        },
+        {
+          "x": "2017",
+          "y": 151
+        },
+        {
+          "x": "2018",
+          "y": 128
+        }
+      ]
+    }
+  ];  
 
 export default Product;
