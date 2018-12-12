959fc78c1289d26bcb84899e95f1743da6fe80ef
Pedro <pedro21fcp@gmail.com>
2018-12-12 15:22:59 +0000

    update

--- client/src/pages/Purchases.js
+++ client/src/pages/Purchases.js
@@ -1,35 +1,159 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import BootstrapTable from 'react-bootstrap-table-next';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
+import { ResponsivePie } from 'nivo';
+import ReactLoading from 'react-loading';
 
 class Purchases extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      message: 'Hello'
+      purchases: [],
+      loadingPrimavera: true,
+      loadingAPI: true
     };
   }
 
   componentDidMount() {
     this.callAPI()
-        .then(body => this.setState({ message: body.message }))
+        .then((res) => this.handleResponse(res))
         .catch(err => console.log(err));
+
+    if (this.props.token !== "") {
+      this.callPrimavera()
+        .then((res) => this.handlePrimaveraResponse(res))
+        .catch(err => console.log(err));
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    // Check if token was updated
+    if( prevProps.token === "" && this.props.token !== "" ) {
+      this.callPrimavera()
+        .then((res) => this.handlePrimaveraResponse(res))
+        .catch(err => console.log(err));
+    }
   }
 
   callAPI = async () => {
+
     const response = await fetch('/purchases');
     const body = await response.json();
-
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
+  };
+  handleResponse(res) {
+      this.setState( {
+        loadingAPI: false
+      } );
   };
 
-  render() {
+  callPrimavera = async () => {
+    var query = JSON.stringify("SELECT Artigo, Quantidade, TotalIliquido FROM LinhasCompras");
+
+    return axios({
+      method: 'post',
+      url: 'http://localhost:2018/WebApi/Administrador/Consulta',
+      crossdomain: true,
+      headers: {
+        'content-type': 'application/json',
+        'authorization': "Bearer " + this.props.token
+      },
+      data: query
+    });
+  };
+  handlePrimaveraResponse(res) {
+    this.setState({
+      purchases: res.data.DataSet.Table,
+      loadingPrimavera: false
+    });
+  };
+
+
+  loading() {
+    return this.state.loadingAPI || this.props.token === "" || this.state.loadingPrimavera;
+  }
+
+  renderLoading() {
     return (
-      <div>
-        { this.state.message }
+      <div style={{
+        width: '8%',
+        height: '8%',
+        position: "absolute",
+        top: '50%',
+        left: '50%',
+        marginLeft: '-4%',
+        marginTop: '-4%',
+      }}>
+        <ReactLoading type={"spinningBubbles"} color={"#00ffbb"} height={'100%'} width={'100%'} />
       </div>
     );
+  }
+
+
+  render() {
+    if( this.loading() )
+      return this.renderLoading();
+
+      const columns = [{
+          dataField: 'Artigo',
+          text: 'Code',
+          sort: true
+      },{
+          dataField: 'Quantidade',
+          text: 'Quantity',
+          sort: true,
+          filter: textFilter({
+            delay: 50,
+            style: {
+              display: 'none'
+            },
+            placeholder: 'Search product',
+            getFilter: (filter) => {
+              this.nameFilter = filter;
+            }
+          })
+        },
+        {
+          dataField: 'TotalIliquido',
+          text: 'Total Price',
+          sort: true
+        }
+      ];
+
+      const defaultSorted = [{
+        dataField: 'Artigo',
+        order: 'asc'
+      }];
+
+      const customTotal = (from, to, size) => (
+        <span className="react-bootstrap-table-pagination-total">
+          Showing { from } to { to } of { size } products
+        </span>
+      );
+
+      const tableOptions = {
+        hideSizePerPage: true,
+        hidePageListOnlyOnePage: true,
+        showTotal: true,
+        paginationTotalRenderer: customTotal
+      };
+
+      const handleSearchInput = (e) => {
+        this.nameFilter(e.target.value);
+      };
+
+      return (
+        <div className="container">
+          <h1>Purchases</h1>
+          <input type="text" className="form-control" placeholder="Search product" onInput={ handleSearchInput }/>
+          <BootstrapTable bootstrap4 striped hover keyField='PurchaseCode' data={ this.state.purchases } columns={ columns } defaultSorted={defaultSorted} pagination={paginationFactory(tableOptions)} filter={filterFactory()}/>
+        </div>
+      );
+
   }
 }
 
