8d688603fcd6cfcae9caf542ca3ff0aade22d808
Daniel Marques <up201503822@fe.up.pt>
2018-12-13 22:13:51 +0000

    Moved saf-t upload to navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,8 +1,46 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import axios from 'axios';
 import './Navbar.css';
 
 class Navbar extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            files: []
+        }
+
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+        this.onChange = this.onChange.bind(this);
+        this.fileUpload = this.fileUpload.bind(this);
+    }
+
+    onFormSubmit(e){
+        e.preventDefault();
+        this.fileUpload(this.state.files)
+        .then((response) => {
+            window.location.reload();
+        })
+        .catch((err) =>  console.log(err) );
+    }
+    
+    onChange(e) {
+        this.setState( { files: e.target.files } )
+    }
+    
+    fileUpload(files){
+        const url = '/uploadSAFT';
+        const formData = new FormData();
+        formData.append('saft', files[0])
+        const config = {
+            headers: {
+                'content-type': 'multipart/form-data'
+            }
+        }
+        return axios.post(url, formData,config);
+    }
 
   render() {
     return (
@@ -19,18 +57,44 @@ class Navbar extends Component {
                         <NavLink activeClassName="active" className="nav-link" exact to="/financial">Financial</NavLink>
                     </li>
                     <li class="nav-item">
-                        <NavLink activeClassName="active" className="nav-link" exact to="/products">Products</NavLink>
+                        <NavLink activeClassName="active" className="nav-link" to="/products">Products</NavLink>
                     </li>
                     <li class="nav-item">
-                        <NavLink activeClassName="active" className="nav-link" exact to="/sales">Sales</NavLink>
+                        <NavLink activeClassName="active" className="nav-link" to="/sales">Sales</NavLink>
                     </li>
                     <li class="nav-item">
-                        <NavLink activeClassName="active" className="nav-link" exact to="/purchases">Purchases</NavLink>
+                        <NavLink activeClassName="active" className="nav-link" to="/purchases">Purchases</NavLink>
                     </li>
                 </ul>
             </div>
+            <div class="navbar-collapse collapse order-3">
+                <div class="navbar-nav ml-auto">
+                    <ul class="navbar-nav">
+                        <li class="nav-item mx-2">
+                            <div class="custom-file">
+                                <input onChange={this.onChange} type="file" class="custom-file-input" id="customFile"/>
+                                <label class="custom-file-label" for="customFile">{this.getLabel()}</label>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <button class="btn btn-primary" type="button" onClick={this.onFormSubmit}>Upload File</button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
     );
+  }
+
+  getLabel() {
+    let label = "";
+    for(let i = 0; i < this.state.files.length; i++)
+        label += this.state.files[i].name + " ";
+
+    if(label === "")
+        label = "Choose a file";
+    
+    return label;
   }
 
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 
 class Home extends Component {
   constructor(props) {
@@ -7,13 +6,7 @@ class Home extends Component {
 
     this.state = {
       message: 'Hello',
-      files: []
     };
-
-
-    this.onFormSubmit = this.onFormSubmit.bind(this);
-    this.onChange = this.onChange.bind(this);
-    this.fileUpload = this.fileUpload.bind(this);
   }
 
   componentDidMount() {
@@ -33,38 +26,8 @@ class Home extends Component {
 
   render() {
     return (
-      <div>
         <p>{ this.state.message }</p>
-        <form onSubmit={this.onFormSubmit}>
-          Select file:
-          <input type="file" onChange={this.onChange} />
-          <button type="submit">Upload File</button>
-        </form>
-      </div>
     );
-  }
-
-  onFormSubmit(e){
-    e.preventDefault();
-    this.fileUpload(this.state.files).then((response) => {
-      this.setState( { message: response.data.message } );
-    });
-  }
-
-  onChange(e) {
-    this.setState( { files: e.target.files } )
-  }
-
-  fileUpload(files){
-    const url = '/uploadSAFT';
-    const formData = new FormData();
-    formData.append('saft', files[0])
-    const config = {
-        headers: {
-            'content-type': 'multipart/form-data'
-        }
-    }
-    return axios.post(url, formData,config);
   }
 }
 
